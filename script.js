e56918570b9f67b7d6df8273809473614834b522
dog-rose <nessrina.brahim@gmail.com>
2026-03-19 00:06:03 +0100

    hello world3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 🔴 Use ONLY the video ID
-const VIDEO_ID = "pZVdQLn_E5w";
+const VIDEO_ID = "0EWcxtOAJE4";
 
 // Build embed URL
 const url = `https://www.youtube.com/embed/${VIDEO_ID}?autoplay=1&mute=1&controls=0&loop=1&playlist=${VIDEO_ID}&modestbranding=1&rel=0`;
